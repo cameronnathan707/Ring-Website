6d891015d3437c7c2492ca4048419bc903a6e8cc
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2021-05-03 15:33:40 -0600

    initial project setup

--- ring-website/src/App.tsx
+++ ring-website/src/App.tsx
@@ -1,29 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   const appName: string = "Nathan Cameron Creations";
-  return (
-    
-    <div className="App">
-      <h1>Welcome to {appName}</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <div className="App">{appName}</div>;
+
+};
 
 export default App;
