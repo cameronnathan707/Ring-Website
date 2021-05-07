49f1f3c53aefb7c2399cab058bc94780efe23b89
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2021-05-06 18:49:45 -0600

    created a style file for the new pages

---
+++ src/components/components.styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: auto;
+    height: auto;
+    border 3px solid black;
+    background: #9e9393;
+    `;
+
+export const Title = styled.h1`
+  display: flex;
+  font-family: Arial, Helvetica, sans-serif;
+  color: black;
+  font-size: 28pt;
+  margin-left: 50px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+export const Heading = styled.h1`
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  font-size: 20pt;
+  margin-left: 50px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
