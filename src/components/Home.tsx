4bbc9922729aab74e47c601d47d49c5d8d5d89de
Nathan Cameron <62361875+cameronnathan707@users.noreply.github.com>
2021-09-13 12:16:48 -0600

    altered image

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ import { withRouter } from 'react-router-dom';
 function Home() {
   return (
     <Wrapper>
-      <img src={logo}/>;
+      <img src={logo} alt="logo" width="100%" height="100%" />;
     </Wrapper>
   );
 }
